985acd7b75c5479017f661d353af546afc9683a4
santimgb98 <santimgb98@email.com>
2025-04-24 22:18:24 +0200

    correction on the border color when you do click on the button

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ $(document).ready(function () {
             $(".img").attr("src","./gatoVolteo.png")
             $("body").attr("background","./gatoVolteo.png")
             $("#content").css("background","rgb(43, 47, 63)")
+            $("#content").css("border-color","rgb(255, 255, 255)")
           } else {
             $(this).text("light mode")
             $(".title").text("kat")
             $(".img").attr("src","./gato.png")
             $("body").attr("background","./gato.png")
             $("#content").css("background","rgb(255, 255, 255)")
+            $("#content").css("border-color","rgb(43, 47, 63)")
 
           }
     })
