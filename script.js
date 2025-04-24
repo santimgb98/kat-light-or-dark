468ace33b7023652091ca3b2b541ebcece122a4e
santimgb98 <santimgb98@email.com>
2025-04-24 22:12:41 +0200

    Click the button, the kat goes crazy

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ $(document).ready(function () {
             $(".title").text("kat volteo")
             $(".img").attr("src","./gatoVolteo.png")
             $("body").attr("background","./gatoVolteo.png")
+            $("#content").css("background","rgb(43, 47, 63)")
           } else {
             $(this).text("light mode")
             $(".title").text("kat")
             $(".img").attr("src","./gato.png")
             $("body").attr("background","./gato.png")
+            $("#content").css("background","rgb(255, 255, 255)")
+
           }
     })
     
